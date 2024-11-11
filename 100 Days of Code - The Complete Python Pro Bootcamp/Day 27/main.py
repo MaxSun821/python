335ff273cb825b062fc3829806836e1439022a86
max <maxsun8216@gmail.com>
2024-11-11 18:31:22 +0800

    GUI

---
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 27/main.py
@@ -0,0 +1,10 @@
+import tkinter
+
+window = tkinter.Tk()
+window.title("My First GUI Program")
+window.minsize(width=800, height=600)
+
+# Label
+my_label = tkinter.Label(text="This is a Label", font=("Arial", 24, "bold"))
+
+window.mainloop()
