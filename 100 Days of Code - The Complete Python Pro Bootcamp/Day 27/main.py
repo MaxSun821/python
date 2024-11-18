7a9db1d14aff4a46117592bbb0a4d6cf0d00dd20
maxsun821 <maxsun8216@gmail.com>
2024-11-18 13:23:19 +0800

    python argument and tkinter

--- 100 Days of Code - The Complete Python Pro Bootcamp/Day 27/main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 27/main.py
@@ -6,5 +6,21 @@ window.minsize(width=800, height=600)
 
 # Label
 my_label = tkinter.Label(text="This is a Label", font=("Arial", 24, "bold"))
-my_label.pack()
+my_label.grid(column=0, row=0)
+
+
+def button_clicked():
+    my_label.config(text=entry.get())
+
+
+button = tkinter.Button(text="Click Me", command=button_clicked)
+button.grid(column=1, row=1)
+
+new_button = tkinter.Button(text="New Button")
+new_button.grid(column=2, row=0)
+
+
+entry = tkinter.Entry(width=10)
+entry.grid(column=3, row=2)
+
 window.mainloop()
