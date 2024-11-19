3d5bc3857dc4c4e7fdc0b8349dbc8e4ec17ba766
max <maxsun8216@gmail.com>
2024-11-19 18:27:48 +0800

    Pomodoro

--- 100 Days of Code - The Complete Python Pro Bootcamp/pomodoro-start/main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/pomodoro-start/main.py
@@ -12,8 +12,18 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    count_down(5 * 60)
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    count_min = count // 60
+    count_sec = count % 60
+    canvas.itemconfig(timer_text, text=f"{count_min:02d}:{count_sec:02d}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -26,15 +36,16 @@ timer_label.grid(column=1, row=0)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 126, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 126, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
-check_label = Label(text="✔", bg=YELLOW, fg=GREEN)
-check_label.grid(column=1, row=2)
 
-start_button = Button(text="Start", bg=YELLOW, highlightthickness=0)
+check_label = Label(text="✔", bg=YELLOW, fg=GREEN)
+check_label.grid(column=1, row=3)
+
+start_button = Button(text="Start", command=start_timer, highlightthickness=0)
 start_button.grid(column=0, row=2)
-reset_button = Button(text="Reset", bg=YELLOW, highlightthickness=0)
+reset_button = Button(text="Reset", highlightthickness=0)
 reset_button.grid(column=2, row=2)
 
 
