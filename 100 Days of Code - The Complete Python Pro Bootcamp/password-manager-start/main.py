65e628b5962b129219b51d1d58511ec7d78408d1
maxsun821 <maxsun8216@gmail.com>
2024-11-20 07:42:07 +0800

    password manager

--- 100 Days of Code - The Complete Python Pro Bootcamp/password-manager-start/main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/password-manager-start/main.py
@@ -1,5 +1,16 @@
+from tkinter import *
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password Manager")
+window.config(pady=20, padx=20)
+
+canvas = Canvas(width=200, height=200)
+pass_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=pass_img)
+canvas.pack()
+
+window.mainloop()
