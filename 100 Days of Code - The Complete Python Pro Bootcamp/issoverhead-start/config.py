a0aabbbd687cfa8bb500ac8f6566960c3241a73e
maxsun821 <maxsun8216@gmail.com>
2024-11-22 13:24:40 +0800

    iss over head

---
+++ 100 Days of Code - The Complete Python Pro Bootcamp/issoverhead-start/config.py
@@ -0,0 +1,7 @@
+MY_LAT = 32.060253
+MY_LONG = 118.796875
+ISS_API = "http://api.open-notify.org/iss-now.json"
+SUNRISE_API = "https://api.sunrise-sunset.org/json"
+SMTP_SERVER = "smtp.gmail.com"
+MY_EMAIL = "my@gmail.com"
+MY_PASSWORD = "abcdefg"
