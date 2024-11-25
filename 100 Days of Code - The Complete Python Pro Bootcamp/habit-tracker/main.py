5e68acece244bf71f7283352283144e99d632d76
maxsun821 <maxsun8216@gmail.com>
2024-11-25 12:44:39 +0800

    pixela

--- 100 Days of Code - The Complete Python Pro Bootcamp/habit-tracker/main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/habit-tracker/main.py
@@ -1,10 +1,22 @@
 import requests
 from config import *
+
 # Create a user
-
-
 # response = requests.post(url=pixela_endpoint, json=user_params)
 # print(response.text)
 
-response = requests.post(url=graph_endpoint, json=graph_params, headers=headers)
+# Create a graph
+# response = requests.post(url=graph_endpoint, json=graph_params, headers=headers)
+# print(response.text)
+
+# Post a pixela
+# response = requests.post(url=record_endpoint, json=record_params, headers=headers)
+# print(response.text)
+
+# Update a pixela
+# response = requests.put(url=update_endpoint, json=update_params, headers=headers)
+# print(response.text)
+
+# Delete a pixela
+response = requests.delete(url=delete_endpoint, headers=headers)
 print(response.text)
