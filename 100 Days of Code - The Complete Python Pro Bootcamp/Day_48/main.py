8fa9443b5ad592deca425363abbbe39e22174429
maxsun821 <maxsun8216@gmail.com>
2024-12-09 20:46:06 +0800

    selenium

--- 100 Days of Code - The Complete Python Pro Bootcamp/Day_48/main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day_48/main.py
@@ -15,8 +15,14 @@ driver.get("https://www.python.org/")
 # price_whole = driver.find_element(By.XPATH, value='//*[@id="corePriceDisplay_desktop_feature_div"]/div[1]/span[3]/span[2]/span[2]')
 # print(price_whole.text)
 
-times = driver.find_elements(By.CSS_SELECTOR, "ul li time")
-for time in times:
-    print(time.text)
+event_times = driver.find_elements(By.CSS_SELECTOR, ".event-widget time")
+event_names = driver.find_elements(By.CSS_SELECTOR, ".event-widget li a")
+events = {}
+for n in range(len(event_times)):
+    events[n] = {
+        "time": event_times[n].text,
+        "name": event_names[n].text
+    }
 
+print(events)
 driver.quit()
