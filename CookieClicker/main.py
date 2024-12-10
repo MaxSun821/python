fd6e80d77f472486147e5aa5d7e7921f9f601324
maxsun821 <maxsun8216@gmail.com>
2024-12-10 20:45:12 +0800

    Cookie Click Project

--- CookieClicker/main.py
+++ CookieClicker/main.py
@@ -10,14 +10,45 @@ chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://orteil.dashnet.org/experiments/cookie/")
 
+items = driver.find_elements(By.CSS_SELECTOR, value="#store div")
+item_ids = [item.get_attribute("id") for item in items]
+
 cookie = driver.find_element(By.ID, value="cookie")
-to_buy = [123456789, 1000000, 50000, 7000, 2000, 500, 100]
-begin = time.time()
+timeout = time.time() +  5
+five_min = time.time() + 5 * 60
+
 while True:
     cookie.click()
-    end = time.time()
-    if end - begin >= 5:
-        cookie_num = driver.find_element(By.ID, value="money")
-        # print(int(cookie_num.text))
-        # break
+    if time.time() > timeout:
+        cookie_num = driver.find_element(By.ID, value="money").text
+        all_price = driver.find_elements(By.CSS_SELECTOR, value="#store b")
+        item_price = []
+
+        for price in all_price:
+            element_text = price.text
+            if element_text != "":
+                cost = int(element_text.split("-")[1].strip().replace(",", ""))
+                item_price.append(cost)
+        cookie_upgrades = {}
+        for n in range(len(item_price)):
+            cookie_upgrades[item_price[n]] = item_ids[n]
+        if "," in cookie_num:
+            cookie_num = cookie_num.replace(",", "")
+        money = int(cookie_num)
+
+        affordable_upgrades = {}
+        for cost, id in cookie_upgrades.items():
+            if money > cost:
+                affordable_upgrades[cost] = id
+        highest_price_affordable_upgrades = max(affordable_upgrades)
+        to_purchase_id = affordable_upgrades[highest_price_affordable_upgrades]
+
+        driver.find_element(By.ID, value=to_purchase_id).click()
+
+        timeout = time.time() + 5
+
+    if time.time() > five_min:
+        cookie_per_s = driver.find_element(By.ID, value="cps").text
+        print(cookie_per_s)
+        break
 driver.quit()
