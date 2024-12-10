975d83610db41d3abf11b5ef85ee209b53c80b71
maxsun821 <maxsun8216@gmail.com>
2024-12-10 13:07:26 +0800

    selenium

--- 100 Days of Code - The Complete Python Pro Bootcamp/Day_48/interaction.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day_48/interaction.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
@@ -7,7 +8,10 @@ chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://en.wikipedia.org/wiki/Main_Page")
 
-article_count = driver.find_element(By.CSS_SELECTOR, value="#articlecount a")
-print(article_count.text)
+# article_count = driver.find_element(By.CSS_SELECTOR, value="#articlecount a")
+# print(article_count.text)
 
-driver.quit()
+search = driver.find_element(By.NAME, "search")
+search.send_keys("Python", Keys.ENTER)
+
+# driver.quit()
--- 100 Days of Code - The Complete Python Pro Bootcamp/Day_48/main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day_48/main.py
@@ -1,11 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
-driver = webdriver.Chrome()
-driver.get("https://www.python.org/")
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("https://secure-retreat-92358.herokuapp.com/")
 
 # price_whole = driver.find_element(By.CLASS_NAME, value="a-price-whole")
 # price_fraction = driver.find_element(By.CLASS_NAME, value="a-price-fraction")
@@ -15,14 +16,24 @@ driver.get("https://www.python.org/")
 # price_whole = driver.find_element(By.XPATH, value='//*[@id="corePriceDisplay_desktop_feature_div"]/div[1]/span[3]/span[2]/span[2]')
 # print(price_whole.text)
 
-event_times = driver.find_elements(By.CSS_SELECTOR, ".event-widget time")
-event_names = driver.find_elements(By.CSS_SELECTOR, ".event-widget li a")
-events = {}
-for n in range(len(event_times)):
-    events[n] = {
-        "time": event_times[n].text,
-        "name": event_names[n].text
-    }
+# event_times = driver.find_elements(By.CSS_SELECTOR, ".event-widget time")
+# event_names = driver.find_elements(By.CSS_SELECTOR, ".event-widget li a")
+# events = {}
+# for n in range(len(event_times)):
+#     events[n] = {
+#         "time": event_times[n].text,
+#         "name": event_names[n].text
+#     }
+#
+# print(events)
 
-print(events)
-driver.quit()
+first_name = driver.find_element(By.NAME, value="fName")
+last_name = driver.find_element(By.NAME, value="lName")
+email = driver.find_element(By.NAME, value="email")
+
+first_name.send_keys("Max")
+last_name.send_keys("Sun")
+email.send_keys("maxsun@123.com")
+button = driver.find_element(By.CSS_SELECTOR, value="button")
+button.click()
+# driver.quit()
