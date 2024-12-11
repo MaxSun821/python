bbd1d5d8ba4c9c75ff7b8abb81ce7897180fb8f5
maxsun821 <maxsun8216@gmail.com>
2024-12-12 07:42:07 +0800

    Auto Tinder Swiping

---
+++ AutoTinderSwiping/main.py
@@ -0,0 +1,23 @@
+from time import sleep
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("https://tinder.com/")
+sleep(2)
+login_button = driver.find_element(By.XPATH, value='//*[@id="t636489831"]/div/div[1]/div/main/div[1]/div/div/div/div/div/header/div/div[2]/div[2]/a')
+login_button.click()
+
+sleep(2)
+fb_login = driver.find_element(By.XPATH, value='//*[@id="t-1091891245"]/div/div[1]/div/div[1]/div/div/div[2]/div[2]/span/div[3]/button')
+fb_login.click()
+
+sleep(5)
+base_window = driver.window_handles[0]
+fb_login_window = driver.window_handles[1]
+driver.switch_to.window(fb_login_window)
+print(driver.title)
