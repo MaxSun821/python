13dd03a81db63cb68d26c94a0117e5024ee9d5b1
maxsun821 <maxsun8216@gmail.com>
2024-12-19 13:23:41 +0800

    Flask

--- 100 Days of Code - The Complete Python Pro Bootcamp/HelloFlask/hello.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/HelloFlask/hello.py
@@ -4,13 +4,30 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World</p>"
 
+def make_bold(function):
+    def wrapper(*args, **kwargs):
+        return f'<b>{function()}</b>'
+    return wrapper
+
+def make_emphasis(function):
+    def wrapper(*args, **kwargs):
+        return f'<em>{function()}</em>'
+    return wrapper
+
+def make_underline(function):
+    def wrapper(*args, **kwargs):
+        return f'<u>{function()}</u>'
+    return wrapper
 @app.route('/bye')
+@make_bold
+@make_emphasis
+@make_underline
 def say_bye():
-    return "<p>Bye</p>"
+    return "Bye"
 
 @app.route('/username/<name>')
 def say_hello(name):
     return f"Hello, {name}!"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
